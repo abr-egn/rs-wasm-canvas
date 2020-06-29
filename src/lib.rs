644108cc760957e452aaa2e4038cead554c5c5ae
Abraham Egnor <abe.egnor@gmail.com>
2020-06-29 09:49:20 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+use log::{Level, info};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn wasm_start() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    console_log::init_with_level(Level::Debug).expect("error initializing console_log");
+
+    info!("WASM initialized.");
+}
