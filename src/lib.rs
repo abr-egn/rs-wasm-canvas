7fc4cca16a0ed32a24725069db12eb122caf21ab
Abraham Egnor <abe.egnor@gmail.com>
2020-06-29 11:25:39 -0400

    work towards rendering; memory not found?

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::PI;
+
 use log::{Level, info};
 use wasm_bindgen::prelude::*;
 
@@ -12,4 +14,33 @@ pub fn wasm_start() {
 #[wasm_bindgen]
 pub fn greet() {
     info!("Hello.");
+}
+
+#[wasm_bindgen]
+pub struct Data {
+    xs: Vec<f32>,
+    ys: Vec<f32>,
+}
+
+const POINTS: usize = 100;
+const RADIUS: f32 = 50.0;
+
+#[wasm_bindgen]
+impl Data {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut xs = vec![];
+        let mut ys = vec![];
+        let inc = (2.0*PI)/(POINTS as f32);
+        for ix in 0..POINTS {
+            let angle = (ix as f32) * inc;
+            xs.push(RADIUS * angle.cos());
+            ys.push(RADIUS * angle.sin());
+        }
+        Data { xs, ys }
+    }
+
+    pub fn count(&self) -> usize { self.xs.len() }
+    pub fn xs(&self) -> *const f32 { self.xs.as_ptr() }
+    pub fn ys(&self) -> *const f32 { self.ys.as_ptr() }
 }
--- www/ts/index.ts
+++ www/ts/index.ts
@@ -1,16 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import {Index} from "../tsx/index";
+import { Index } from "../tsx/index";
 import * as wasm from "../wasm";
+import { memory } from "../wasm/index_bg";
 
 function main() {
-    initCanvas();
+    const canvas = initCanvas();
     renderReact();
-    wasm.greet();
+
+    const data = new wasm.Data();
+    window.requestAnimationFrame((ts) => drawFrame(canvas, data, ts));
 }
 
-function initCanvas() {
+function initCanvas(): HTMLCanvasElement {
     const canvas = document.getElementById("render") as HTMLCanvasElement;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -22,11 +25,29 @@ function initCanvas() {
         console.info("Key pressed: ", e.code);
     });
     canvas.focus();
+    return canvas;
 }
 
 function renderReact() {
     let element = React.createElement(Index, {}, null);
     ReactDOM.render(element, document.getElementById("root"));
+}
+
+function drawFrame(canvas: HTMLCanvasElement, data: wasm.Data, ts: number) {
+    // TODO: data.update()
+
+    const count = data.count();
+    const xs = new Float32Array(memory.buffer, data.xs(), count);
+    const ys = new Float32Array(memory.buffer, data.ys(), count);
+
+    canvas.width = canvas.width;  // clear
+    const ctx = canvas.getContext("2d")!;
+    ctx.fillStyle = "#FFFFFF";
+    for (let ix = 0; ix < count; ix++) {
+        ctx.fillRect(xs[ix], ys[ix], 5, 5);
+    }
+
+    window.requestAnimationFrame((ts) => drawFrame(canvas, data, ts));
 }
 
 main();
